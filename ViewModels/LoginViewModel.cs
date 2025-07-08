aedcdf69ef4248674876cbcd89833d878705711b
Nadiya-badusha <nadiyabadusha129@gmail.com>
2025-07-08 16:17:12 +0530

    Add files via upload

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Student_Record_Management_System.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required.")]
+
+        public string Password { get; set; }
+    }
+
+}
